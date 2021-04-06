8e03d081f0d50f7ddfbb144fe415a3ae941273aa
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-04-06 23:13:14 +0000

    Added change and submit function to advanced search page. listening for clicks on all buttons now. #1

--- client/pages/advanced-search.jsx
+++ client/pages/advanced-search.jsx
@@ -8,34 +8,55 @@ export default class AdvancedSearch extends React.Component {
       inputAuthorValue: '',
       inputIsbnValue: ''
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    if (name === 'title') {
+      this.setState({ inputTitleValue: event.target.value });
+
+    } else if (name === 'author') {
+      this.setState({ inputAuthorValue: event.target.value });
+
+    } else if (name === 'ISBN') {
+      this.setState({ inputIsbnValue: event.target.value });
+
+    }
+
   }
 
   render() {
     return (
       <div className="search-container advanced">
-        <div className="heading">Advanced Search</div>
-        <form>
+        <div className="adv-heading">Advanced Search</div>
+        <form className="search-form" onSubmit={this.handleSubmit}>
           <label>
-            <div className="headingTwo">Search by Title</div>
-            <input type="text" placeholder="Title Name..." required />
+            <div className="search-heading">Search by Title</div>
+            <input name="title" className="text-box" type="text" placeholder="Title Name..." required onChange={this.handleChange} />
             <div>
               <input className="button submit" type="submit" value="Submit" />
             </div>
           </label>
         </form>
-        <form>
+        <form className="search-form" onSubmit={this.handleSubmit}>
           <label>
-            <div className="headingTwo">Search by Author</div>
-            <input type="text" placeholder="Author Name..." required />
+            <div className="search-heading">Search by Author</div>
+            <input name="author" className="text-box" type="text" placeholder="Author Name..." required onChange={this.handleChange} />
             <div>
               <input className="button submit" type="submit" value="Submit" />
             </div>
           </label>
         </form>
-        <form>
+        <form className="search-form" onSubmit={this.handleSubmit}>
           <label>
-            <div className="headingTwo">Search by ISBN</div>
-            <input type="text" placeholder="ISBN number..." required />
+            <div className="search-heading">Search by ISBN</div>
+            <input name="ISBN" className="text-box" type="text" placeholder="ISBN number..." required onChange={this.handleChange} />
             <div>
               <input className="button submit" type="submit" value="Submit" />
             </div>
