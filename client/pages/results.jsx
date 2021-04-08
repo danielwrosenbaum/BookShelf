cf6d5c68c466c12d683c837ee341d443b8fbfc02
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-04-08 00:12:00 +0000

    Added heart and plus icons to cards. used flexbox on cards to align items appropriately and account for mobile & dekstop. #2

--- client/pages/results.jsx
+++ client/pages/results.jsx
@@ -23,12 +23,20 @@ export default class Results extends React.Component {
             const year = parseInt(books.volumeInfo.publishedDate, 10);
             return (
               <div key={index} className="card">
-                <img className="thumbnail" src={thumbNail} alt={title} />
-                <div className="book-info">
-                  <h2 className="heading three no-top">{title}</h2>
-                  <h3 className="heading three no-bottom">by {author}</h3>
-                  <div className="heading three">{year}</div>
-                  <button className="info button">More Info</button>
+                <div className="result-info">
+                  <img className="thumbnail" src={thumbNail} alt={title} />
+                  <div className="book-info">
+                    <h2 className="heading three no-top">{title}</h2>
+                    <h3 className="heading three no-bottom">by {author}</h3>
+                    <div className="heading three">{year}</div>
+                    <button className="info button">More Info</button>
+                  </div>
+
+                </div>
+                <div className="card-icons">
+                  <i className="fas fa-plus"></i>
+                  <i className="far fa-heart"></i>
+
                 </div>
 
               </div>
