1213abab864844c8774bdfa3d41ee83556fa19dd
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-04-09 23:19:42 +0000

    created database for library. #4

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,18 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+create table "public"."library" (
+  "libraryId"           serial,
+  "title"               text          not null,
+  "author"              text          not null,
+  "isbn"                text          not null,
+  "genre"               text,
+  "year"                integer       not null,
+  "pages"               integer,
+  "googleId"            text          not null,
+  "coverUrl"            text,
+  "addedAt"             timestamptz(6) not null default now(),
+  primary key ("libraryId"),
+  unique ("googleId")
+);
