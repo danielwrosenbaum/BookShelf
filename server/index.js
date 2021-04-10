21e5e1fc6d12a3075d68fd1f6865656e37b9ddfe
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-04-10 00:01:32 +0000

    add error middleware, client-error, and db file. changed schema to simplify database. #4

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 require('dotenv/config');
+// const pg = require('pg');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
+const errorMiddleware = require('./error-middleware');
 
 const app = express();
 
 app.use(staticMiddleware);
+app.post('/api/library', (req, res, next) => {
 
+});
+app.use(errorMiddleware);
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
