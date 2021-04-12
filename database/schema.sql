b5147a732d1d1915b3b1d211c2793fe90be00bbf
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-04-12 22:42:49 +0000

    added rating info to library table. #6

--- database/schema.sql
+++ database/schema.sql
@@ -12,6 +12,7 @@ create table "public"."library" (
   "author"              text,
   "googleId"            text          not null,
   "coverUrl"            text,
+  "hearts"              integer,
   "addedAt"             timestamptz(6) not null default now(),
   primary key ("libraryId"),
   unique ("googleId")
