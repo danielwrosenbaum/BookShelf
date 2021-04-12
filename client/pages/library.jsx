14d0877fddde7dcb457acaf5ee3b5ed25afdb942
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-04-12 18:07:35 +0000

    create library page. #5

---
+++ client/pages/library.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Library extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="library-container"></div>
+    );
+  }
+}
