028b42e27d730b0ef650893fef7f9b6ce9d58abf
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-04-13 22:43:52 +0000

    added container, header and function to get info from database to reading-list. #8

--- client/pages/reading-list.jsx
+++ client/pages/reading-list.jsx
@@ -18,13 +18,49 @@ export default class ReadingList extends React.Component {
   }
 
   render() {
+    const { result } = this.state;
+    if (!result) return null;
+    const books = result;
+    const bookResults = (
+      <div className="reading-list-container">
+        {
+          books.map((book, index) => {
+            const title = book.title;
+            const thumbNail = book.coverUrl;
+            const googleId = book.googleId;
+            const author = book.author;
+            return (
+              <div key={index} id={googleId} className="library-card">
+                <a className="no-underline library-click" href={`#details?bookId=${googleId}`}>
+                  <div className="lib-info no-outline">
+                    <div className='pic-container'>
+                      <img className="thumbnail" src={thumbNail} alt={title} />
+                    </div>
+
+                    <div className="lib-col">
+                      <div>
+                        <div className="heading six">{title}</div>
+                        <div className="heading three no-top no-bottom lib-author">by {author}</div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <div className="delete-container">
+                  X
+                  </div>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
     return (
       <>
         <div className="details-title">
           <div className="heading two-white">Reading List</div>
         </div>
         <div className="reading-list-page">
-          {/* {bookResults} */}
+          {bookResults}
         </div>
       </>
     );
