97a1ee8d2661d13e04e03bb3a7d941e962d17f39
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-04-16 19:19:27 +0000

    update schema to reflect new tables. #27

--- database/schema.sql
+++ database/schema.sql
@@ -6,25 +6,46 @@ drop schema "public" cascade;
 
 create schema "public";
 
-create table "public"."library" (
-  "libraryId"           serial,
+
+create table "public"."users" (
+  "userId"              serial,
+  "username"            text          not null,
+  "hashedPassword"      text          not null,
+  "createdAt"           timestamptz(6)  not null default now(),
+  primary key ("userId"),
+  unique ("username")
+);
+
+create table "public"."books" (
+  "bookId"              serial,
   "title"               text          not null,
   "author"              text,
   "googleId"            text          not null,
   "coverUrl"            text,
-  "stars"              integer,
   "addedAt"             timestamptz(6) not null default now(),
-  primary key ("libraryId"),
+  primary key ("bookId"),
   unique ("googleId")
 );
 
 create table "public"."readingList" (
-  "readingListId"           serial,
-  "title"               text          not null,
-  "author"              text,
-  "googleId"            text          not null,
-  "coverUrl"            text,
-  "addedAt"             timestamptz(6) not null default now(),
-  primary key ("readingListId"),
-  unique ("googleId")
+  "userId"               integer not null,
+  "googleId"             text not null,
+  "rating"               integer,
+  "isRead"               boolean,
+   primary key ("userId", "googleId"),
+   foreign key ("userId")
+    references "users" ("userId"),
+   foreign key ("googleId")
+    references "books" ("googleId")
 );
+
+-- create table "public"."readingList" (
+--   "readingListId"           serial,
+--   "title"               text          not null,
+--   "author"              text,
+--   "googleId"            text          not null,
+--   "coverUrl"            text,
+--   "addedAt"             timestamptz(6) not null default now(),
+--   primary key ("readingListId"),
+--   unique ("googleId")
+-- );
