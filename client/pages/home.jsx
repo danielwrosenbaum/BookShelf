82cd39eb7e938012cab0287f37a5e8c6be11cd50
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-08 01:04:50 +0000

    worked on home page. added file-loader to webpack. added image to home page. #9

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import SearchPage from './search-page';
+// import SearchPage from './search-page';
 import AppContext from '../lib/app-context';
 import Redirect from '../components/redirect';
+import bookCover from '../../server/public/book-cover.jpeg';
 
 export default class Home extends React.Component {
   render() {
     if (!this.context.user) return <Redirect to="sign-in" />;
 
     return (
-        <SearchPage />
+        // <SearchPage />
+        <div className="home-page">
+        {/* <div className="home-title">BookShelf</div> */}
+          <div className="img-container">
+          <img className="book-cover" src={bookCover} alt="book" />
+          </div>
+
+        </div>
     );
   }
 
