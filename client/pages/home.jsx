d87b6909a05353be04d984f59588f0547898625a
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-08 22:16:31 +0000

    added timer to change home page after 2 seconds to either sign in or search page. #9

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -4,11 +4,54 @@ import AppContext from '../lib/app-context';
 import Redirect from '../components/redirect';
 
 export default class Home extends React.Component {
-  render() {
-    if (!this.context.user) return <Redirect to="sign-in" />;
+  // _isMounted = false;
+  constructor(props) {
+    super(props);
+    this.state = {
+      logoChange: false
+    };
+  }
 
+  componentDidMount() {
+    const { logoChange } = this.state;
+    if (!logoChange) {
+      this.changePage = setTimeout(() => {
+        this.setState({ logoChange: true });
+      }, 3000);
+    }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.changePage);
+  }
+
+  changePage() {
+    const timer = setTimeout(() => {
+      this.setState({ logoChange: true });
+    }, 2000);
+    return timer;
+  }
+
+  renderChange() {
+    const { logoChange } = this.state;
+    if (!logoChange) {
+      this.changePage();
+    }
+
+  }
+
+  render() {
+    const { logoChange } = this.state;
+    if (logoChange) {
+      if (!this.context.user) {
+        return <Redirect to="sign-in" />;
+      } else {
+        return <Redirect to="search-page" />;
+      }
+    }
     return (
       <div className="home-page">
+        {/* {this.renderChange()} */}
         <div className="home-title-container">
           <div className="home-title">BookShelf</div>
           <div className="home-description">Your Own Personal Library</div>
