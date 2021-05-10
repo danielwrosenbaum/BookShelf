47d41c65b37baf57a9d45f334933d544cf7265c1
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-10 20:20:36 +0000

    added header to Reading Plan Page. added CSS transitions to page. added isLoading to state. #11

--- client/pages/reading-plan.jsx
+++ client/pages/reading-plan.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
+import Header from '../components/header';
 
 export default class ReadingPlan extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading: true
 
     };
   }
 
   render() {
+    const { isLoading } = this.state;
     return (
-      <div className="reading-plan-page"></div>
+       <>
+        <Header />
+        <div className="details-title">
+          <div className="heading two-white">Reading Plan</div>
+        </div>
+        <CSSTransition
+          in={!isLoading}
+          appear={true}
+          timeout={500}
+          classNames="fade">
+          <div className="reading-plan-page">
+          </div>
+        </CSSTransition>
+      </>
 
     );
   }
