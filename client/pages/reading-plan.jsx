d03bb49278258749fc02b1b0d2f8d8557f7f4852
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-05-10 20:54:20 +0000

    added map function to reading plan page to take all books from reading list and render them into drop-down select box to populate reading plan form. #11

--- client/pages/reading-plan.jsx
+++ client/pages/reading-plan.jsx
@@ -1,18 +1,84 @@
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 import Header from '../components/header';
+import AppContext from '../lib/app-context';
 
 export default class ReadingPlan extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true
-
+      isLoading: true,
+      networkError: false,
+      result: null
     };
+  }
+
+  componentDidMount() {
+    const { user } = this.context;
+    if (!user) return null;
+    const userId = user.userId;
+    fetch(`/api/bookShelf/readingList/${userId}`)
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          result,
+          isLoading: false
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        if (error) {
+          this.setState({
+            isLoading: false,
+            networkError: true
+          });
+        }
+      });
+  }
+
+  renderReadingListBooks() {
+    const { result } = this.state;
+    if (!result) return null;
+    const bookResults = (
+      <>
+      <label htmlFor="books">Choose A Book</label>
+      <select id="books" name="books" className="reading-plan-container">
+        {(result.length === 0) &&
+          <option value="none" className="title two">No Books In Reading List!</option>}
+          {
+            result.map(book => {
+              const title = book.title;
+              const bookId = book.bookId;
+              return (
+                <option key={bookId} value={bookId}>{title}</option>
+              );
+            })
+          }
+      </select>
+      </>
+    );
+    return bookResults;
   }
 
   render() {
     const { isLoading } = this.state;
+    const { result } = this.state;
+    if (!result) return null;
+    const bookResults = (
+        <select id="books" name="books" className="reading-plan-container text-box">
+          {(result.length === 0) &&
+            <option value="none" className="title two">No Books In Reading List!</option>}
+          {
+            result.map(book => {
+              const title = book.title;
+              const bookId = book.bookId;
+              return (
+                <option key={bookId} value={bookId}>{title}</option>
+              );
+            })
+          }
+        </select>
+    );
     return (
        <>
         <Header />
@@ -25,6 +91,13 @@ export default class ReadingPlan extends React.Component {
           timeout={500}
           classNames="fade">
           <div className="reading-plan-page">
+            <div className="reading-plan-container">
+            <form className="reading-plan-form">
+              <label>Choose A Book</label>
+              {bookResults}
+              <input className="button submit" type="submit" />
+            </form>
+            </div>
           </div>
         </CSSTransition>
       </>
@@ -32,3 +105,5 @@ export default class ReadingPlan extends React.Component {
     );
   }
 }
+
+ReadingPlan.contextType = AppContext;
